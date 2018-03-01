8dbafe322cc18e623ef200f352d228f1866e2309
Florian Koch <fkoch@selectline.de>
2018-03-01 15:16:37 +0100

    created sample application

---
+++ LayoutTest/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Core;
+
+namespace LayoutTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnAppStartup_UpdateThemeName(object sender, StartupEventArgs e)
+        {
+            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName();
+
+            var mainWindow1 = new MainWindow();
+            mainWindow1.Title = "Without SnapsToDevicePixel";
+            mainWindow1.Show();
+
+            var mainWindow2 = new MainWindow();
+            mainWindow2.Title = "With SnapsToDevicePixel";
+            mainWindow2.SnapsToDevicePixels = true;
+            mainWindow2.Show();
+        }
+    }
+}
---
+++ LayoutTest/ButtonPanel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LayoutTest
+{
+    public partial class ButtonPanel
+    {
+        public static readonly DependencyProperty IsUpDownProperty = DependencyProperty.Register(
+            nameof(IsUpDown),
+            typeof(bool),
+            typeof(ButtonPanel));
+
+        public bool IsUpDown
+        {
+            get => (bool) GetValue(IsUpDownProperty);
+            set => SetValue(IsUpDownProperty, value);
+        }
+
+        public ButtonPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
